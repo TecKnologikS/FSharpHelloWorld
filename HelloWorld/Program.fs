0e28672bbc37ca5bf04ade0a1b203355e309c536
Robin <robinpauquet@gmail.com>
2016-10-21 09:22:00 +0200

    First Commit

---
+++ HelloWorld/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+
+[<EntryPoint>]
+let main argv = 
+    (* Ceci est un commentaire comme les /* */ *)
+    printfn "Bonjour tout le Monde" //Affiche le texte, 2e style de commentaire
+    let test = Console.ReadLine() //Attend que l'utilisateur appuye sur entrée
+    1 //retourne le code 1
